c8a70ab880a762503544ebef0710ad745edf35fc
Wouter <wouter@wrdx.nl>
2018-04-10 15:49:14 +0200

    Added post-install setup

---
+++ app/Console/Command/InstallShell.php
@@ -0,0 +1,84 @@
+<?php
+
+class InstallShell extends AppShell {
+
+    private $_writable_dirs = [
+        'tmp',
+        'tmp/cache',
+        'tmp/cache/models',
+        'tmp/cache/persistent',
+        'tmp/cache/views',
+        'tmp/logs',
+        'tmp/sessions',
+        'tmp/tests',
+    ];
+
+    private $_rootDir = '';
+
+    public function postInstall() {
+
+        $this->_rootDir = dirname(dirname(__DIR__));
+
+        $this->out('Setup');
+        $this->hr();
+
+        $this->out("\n");
+
+        # /app/tmp permissions
+        $this->out("\n<info>*</info> Changing permissions for tmp");
+
+        foreach ($this->_writable_dirs as $writable_dir) {
+            $path = $this->_rootDir . '/' . $writable_dir;
+            if ( ! chmod($path, 0777)) {
+                $this->out("<error>Failed</error> to change permissions for " . $path);
+            }
+        }
+
+        # Security.salt
+        $salt = hash('sha256', Security::randomBytes(64));
+        $this->_replacePlaceholder('Config/core.php', '__SECURITY_SALT__', $salt, 'Security.salt');
+
+        # Security.cipherSeed
+        $this->_replacePlaceholder('Config/core.php', '__SECURITY_CIPHERSEED__', $this->_cipher(), 'Security.cipherSeed');
+
+        # Security.cipherSeed
+        $key = hash('sha256', Security::randomBytes(64));
+        $this->_replacePlaceholder('Config/core.php', '__SECURITY_KEY__', $key, 'Security.key');
+
+        $this->out("\nDone!\n");
+    }
+
+    private function _replacePlaceholder($file, $search, $replace, $pretty_name) {
+
+        $path = $this->_rootDir . '/' . $file;
+
+        $content = file_get_contents($path);
+
+        $content = str_replace($search, $replace, $content, $count);
+
+        if ($count == 0) {
+            $this->out('<error>Failed</error> to replace placeholder for ' . $pretty_name);
+
+            return;
+        }
+
+        $result = file_put_contents($path, $content);
+        if ($result) {
+            $this->out('<info>*</info> Updated ' . $pretty_name . ' value in ' . $file);
+
+            return;
+        }
+
+        $this->out('<error>Failed</error> to update ' . $pretty_name . ' value');
+    }
+
+    private function _cipher() {
+        $n = '';
+        for ($i = 0; $i < 32; $i++) {
+            $n .= mt_rand(0, 9);
+        }
+
+        return $n;
+    }
+
+}
